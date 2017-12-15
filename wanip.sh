486b50f2a4cc1a612c4fde586acbd9ced1f506be
sempervent <jngrant9@gmail.com>
2017-12-15 15:15:41 -0500

    added check for wanip.sh

--- wanip.sh
+++ wanip.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-dig +short myip.opendns.com @resolver1.opendns.com
+if hash dig 2>/dev/null; then
+   dig +short myip.opendns.com @resolver1.opendns.com
+else
+   echo -e 'Please type "sudo apt install -y dnsutils to enable dig"'
+fi
